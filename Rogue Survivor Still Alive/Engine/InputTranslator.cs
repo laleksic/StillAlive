c747b3c86b7c78bb8bb81cddf50e27a4696d7ffd
Luka Aleksić <aleksicluka@protonmail.com>
2023-08-18 13:16:46 +0200

    Fix for shift/ctrl + number shortcuts

--- Rogue Survivor Still Alive/Engine/InputTranslator.cs
+++ Rogue Survivor Still Alive/Engine/InputTranslator.cs
@@ -25,31 +25,27 @@ namespace djack.RogueSurvivor.Engine
             // slots keys are always used with Ctrl, Shift or Alt modifiers so they are unbound in keybindings.
             if (key.Control || key.Alt || key.Shift)
             {
-                // clear modifiers.
-                key.Control = false;
-                key.Alt = false;
-                key.Shift = false;
-
                 // slot key?
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_0))
+                var code = key.Code;
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_0).Code)
                     return PlayerCommand.ITEM_SLOT_0;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_1))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_1).Code)
                     return PlayerCommand.ITEM_SLOT_1;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_2))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_2).Code)
                     return PlayerCommand.ITEM_SLOT_2;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_3))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_3).Code)
                     return PlayerCommand.ITEM_SLOT_3;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_4))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_4).Code)
                     return PlayerCommand.ITEM_SLOT_4;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_5))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_5).Code)
                     return PlayerCommand.ITEM_SLOT_5;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_6))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_6).Code)
                     return PlayerCommand.ITEM_SLOT_6;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_7))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_7).Code)
                     return PlayerCommand.ITEM_SLOT_7;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_8))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_8).Code)
                     return PlayerCommand.ITEM_SLOT_8;
-                if (key == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_9))
+                if (code == RogueGame.KeyBindings.Get(PlayerCommand.ITEM_SLOT_9).Code)
                     return PlayerCommand.ITEM_SLOT_9;
             }
 
